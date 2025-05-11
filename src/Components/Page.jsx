a11023a6a3c70ca130f39172bb93ed6455a618ee
Kumar Anand <kranand9430@gmail.com>
2025-05-11 11:57:53 +0530

    Page Structured and tailwind installed

---
+++ src/Components/Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div className='bg-[#121212]'>
+    <h1>This is the Page Section</h1>
+    </div>
+  )
+}
+
+export default Page
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Error404 from './Components/Error404.jsx'
+import Page from './Components/Page.jsx'
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Page />,
+      },
+      {
+        path: '*',
+        element: <Error404 />,
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
